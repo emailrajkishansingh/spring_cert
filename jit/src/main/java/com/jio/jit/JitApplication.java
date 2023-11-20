a78654c42692359b4853b8460e9667c905447bb4
Raj <emailrajkishansingh@gmail.com>
2023-11-20 18:35:10 +0530

    sample springboot application

---
+++ jit/src/main/java/com/jio/jit/JitApplication.java
@@ -0,0 +1,13 @@
+package com.jio.jit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JitApplication.class, args);
+	}
+
+}
